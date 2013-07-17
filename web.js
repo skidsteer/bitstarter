d489c329c3b77f36ec05cf84420678ca412af17f
John Smith <example@stanford.edu>
2013-07-16 23:52:35 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-var fs = require('fs');
-var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+var content = fs.readFileSync('index.html');
 
+app.get('/', function(request, response) {
+  response.send(content.toString("utf-8"));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
